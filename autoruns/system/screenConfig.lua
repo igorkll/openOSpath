6da6802d510cff791f2cf559b0b800a61cbfd6e6
igorkll <69525332+igorkll@users.noreply.github.com>
2022-02-18 10:02:43 +0300

    Add files via upload

--- autoruns/system/screenConfig.lua
+++ autoruns/system/screenConfig.lua
@@ -1,65 +1,69 @@
-local fs = require("filesystem")
-local term = require("term")
 local component = require("component")
-local su = require("superUtiles")
-local unicode = require("unicode")
+local event = require("event")
 local term = require("term")
-local tty = require("tty")
+local computer = require("computer")
+local fs = require("filesystem")
+local su = require("superUtiles")
 
------------------------------------------------------
+---------------------------------------------
+
+local connectedScreen
+local connectedGpu
+local function setGS(screen, gpu)
+    component.invoke(gpu, "bind", screen)
+    component.setPrimary("gpu", gpu)
+    term.bind(component.proxy(gpu))
+    component.setPrimary("screen", screen)
+    connectedScreen = screen
+    connectedGpu = gpu
+end
+
+local function getDeviceLevel(address)
+    return tonumber(computer.getDeviceInfo()[address].width)
+end
+
+local function getMax(filter)
+    local maxlevel = 0
+    local deviceaddress = nil
+    local finded = false
+
+    while true do
+        finded = false
+        for address in component.list(filter) do
+            local level = getDeviceLevel(address)
+            if level > maxlevel then
+                maxlevel = level
+                deviceaddress = address
+                finded = true
+                break
+            end
+        end
+        if not finded then
+            return deviceaddress
+        end
+    end
+end
+
+---------------------------------------------
 
 local screenpath = "/etc/screen.cfg"
 local gpupath = "/etc/gpu.cfg"
 
------------------------------------------------------
-
-local systemscreen = (fs.exists(screenpath) and su.getFile(screenpath)) or "*"
-local systemgpu = (fs.exists(gpupath) and su.getFile(gpupath)) or "*"
-
-systemscreen = component.proxy(systemscreen)
-systemgpu = component.proxy(systemgpu)
-
-if systemscreen then
-    component.setPrimary("screen", systemscreen.address)
-else
-    local screen = component.list("screen")()
-    if screen then
-        su.saveFile(screenpath, screen)
-    else
-        fs.remove(screenpath)
-    end
-end
-
-if systemgpu then
-    component.setPrimary("gpu", systemgpu.address)
-    systemgpu.bind(systemscreen.address)
-else
-    local gpu = component.list("gpu")()
-    if gpu then
-        su.saveFile(gpupath, gpu)
-    else
-        fs.remove(gpupath)
-    end
-end
-
------------------------------------------------------
-
-local oldProxy = component.proxy
-local function newProxy(address)
-    local proxy, err = oldProxy(address)
-    if not proxy then return nil, err end
-    if proxy.type == "screen" then
-        local screengpu
-        for address in component.list("gpu") do
-            local gpu = oldProxy(address)
-            if gpu.getScreen() == proxy.address then
-                screengpu = gpu
-            end
-        end
-        if screengpu then
-            proxy.gpu = screengpu
+local function reconnect()
+    if fs.exists(screenpath) and fs.exists(gpupath) then
+        local screen = component.proxy(assert(su.getFile(screenpath)))
+        local gpu = component.proxy(assert(su.getFile(gpupath)))
+        if screen and gpu then
+            setGS(screen.address, gpu.address)
+            return
         end
     end
-    return proxy
+
+    local maxgpu = getMax("gpu")
+    local maxscreen = getMax("screen")
+    setGS(maxscreen, maxgpu)
+    su.saveFile(screenpath, maxscreen)
+    su.saveFile(gpupath, maxgpu)
 end
-component.proxy = newProxy
+
+reconnect()
