bd598bc5c64ad3486d9cd4cb100e9d13fb6121ef
igorkll <69525332+igorkll@users.noreply.github.com>
2022-02-26 13:04:05 +0300

    Add files via upload

---
+++ usr/bin/market.lua
@@ -0,0 +1,185 @@
+local event = require("event")
+local su = require("superUtiles")
+local component = require("component")
+local fs = require("filesystem")
+local wget = loadfile("/bin/wget.lua")
+local term = require("term")
+local process = require("process")
+local unicode = require("unicode")
+local originalInterrupt = process.info().data.signal
+local gui = require("gui_new").create()
+if not component.isAvailable("internet") then
+    print("internet card is not found")
+    print("press enter to continue...")
+    event.pull("key_down", gui.keyboard, nil, 28)
+    return
+end
+local internet = component.internet
+
+--------------------------------------------------
+
+local appsListUrl
+local installFolder
+local gpu = gui.gpu
+local rx, ry = gpu.getResolution()
+
+--------------------------------------------------
+
+local function executeInZone(wait, func, ...)
+    local oldScene = gui.getScene()
+    local oldInterrupt = process.info().data.signal
+
+    gui.select(0)
+    local oldb = gpu.setBackground(0)
+    local oldf = gpu.setForeground(0xFFFFFF)
+    gpu.setResolution(gpu.maxResolution())
+    local rx, ry = gpu.getResolution()
+    gpu.fill(1, 1, rx, ry, " ")
+
+    process.info().data.signal = originalInterrupt
+    local out = {pcall(func, ...)}
+    if not out[1] then print(out[2] or "unkown") end
+    process.info().data.signal = oldInterrupt
+
+    if wait then
+        print("press enter to continue...")
+        while true do
+            local eventName, uuid, _, code = event.pull()
+            if eventName == "key_down" and uuid == term.keyboard() and code == 28 then
+                break
+            end
+        end
+    end
+
+    gpu.setBackground(oldb)
+    gpu.setForeground(oldf)
+
+    gui.select(oldScene or 0)
+    return table.unpack(out)
+end
+
+local function runCommand(command, wait)
+    executeInZone(wait, os.execute, command)
+end
+
+local function runFile(path, wait, ...)
+    executeInZone(wait, shell.execute, path, _ENV, ...)
+end
+
+--------------------------------------------------
+
+local reindex, main
+
+local startZone = gui.createScene()
+startZone.createButton(1, 1, 8, 1, "apps", nil, nil, nil, nil, nil, nil, function()
+    appsListUrl = "https://raw.githubusercontent.com/igorkll/appMarket2/main/list.txt"
+    installFolder = "/usr/bin"
+    if reindex() then return end
+    gui.select(main)
+end)
+startZone.createButton(1, 2, 8, 1, "libs", nil, nil, nil, nil, nil, nil, function()
+    appsListUrl = "https://raw.githubusercontent.com/igorkll/appMarket2/main/lib/list.txt"
+    installFolder = "/usr/lib"
+    if reindex() then return end
+    gui.select(main)
+end)
+startZone.createButton(1, 3, 8, 1, "rc", nil, nil, nil, nil, nil, nil, function()
+    appsListUrl = "https://raw.githubusercontent.com/igorkll/appMarket2/main/rc/list.txt"
+    installFolder = "/etc/rc.d"
+    if reindex() then return end
+    gui.select(main)
+end)
+
+local drawIndex = su.generateRandomID()
+main = gui.createScene()
+main.createDrawZone(1, 1, rx, ry, function() end, drawIndex)
+main.createButton(1, ry - 2, rx, 3, "back", nil, nil, nil, nil, nil, nil, function()
+    gui.select(startZone)
+end)
+local logZone = main.createLogZone(1, 1, rx, ry - 3, nil, nil, nil, nil, false)
+logZone.autodraw = false
+
+gui.select(startZone)
+
+--------------------------------------------------
+
+local function split(str, sep)
+    local parts, count = {}, 1
+    for i = 1, unicode.len(str) do
+        local char = unicode.sub(str, i, i)
+        if not parts[count] then parts[count] = "" end
+        if char == sep then
+            count = count + 1
+        else
+            parts[count] = parts[count] .. char
+        end
+    end
+    return parts
+end
+
+local function getInternetFile(url)
+    local handle, data, result, reason = internet.request(url), ""
+    if handle then
+        while true do
+            result, reason = handle.read(math.huge) 
+            if result then
+                data = data .. result
+            else
+                handle.close()
+                
+                if reason then
+                    return nil, reason
+                else
+                    return data
+                end
+            end
+        end
+    else
+        return nil, "unvalid address"
+    end
+end
+
+local indexed = {}
+
+function reindex()
+    logZone.clear()
+    local appsList, err = getInternetFile(appsListUrl)
+    if not appsList then
+        gui.splash("произошла ошибка, возможно нет соенденения с интернетом")
+        return true
+    end
+    local strs = split(appsList, "\n")
+    for i = 1, #strs do
+        local url, name = table.unpack(split(strs[i], ";"))
+        indexed[name] = url
+        logZone.add(name)
+    end
+    logZone.draw()
+end
+
+--------------------------------------------------
+
+local lastTouchX, lastTouchY
+
+while true do
+    local eventData = {event.pull()}
+    if eventData[1] == "touch" and eventData[2] == term.screen() then
+        lastTouchX = eventData[3]
+        lastTouchY = eventData[4]
+    end
+    gui.uploadEvent(table.unpack(eventData))
+    if eventData[1] == "drawZone" and eventData[2] == "touch" and eventData[3] == drawIndex then
+        local posX = eventData[4]
+        local posY = eventData[5]
+        local text = logZone.strs[posY]
+        local url = indexed[text]
+
+        if eventData[6] == 0 then
+            if url and gui.yesno("вы уверенны что хотите устоновить "..text.."?") then
+                fs.makeDirectory(installFolder)
+                executeInZone(true, wget, url, fs.concat(installFolder, text), "-f")
+                if shell_reindex then shell_reindex() end
+            end
+        end
+    end
+end
