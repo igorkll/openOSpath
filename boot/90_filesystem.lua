2172c09de28dad779946d794c038c73e73ea8341
igorkll <69525332+igorkll@users.noreply.github.com>
2022-03-15 20:04:36 +0300

    Add files via upload

--- boot/90_filesystem.lua
+++ boot/90_filesystem.lua
@@ -5,52 +5,64 @@ local tmp = require("computer").tmpAddress()
 
 local pendingAutoruns = {}
 
-local function onComponentAdded(_, address, componentType)
-  if componentType == "filesystem" and tmp ~= address then
+local function autorun(_, address)
     local proxy = fs.proxy(address)
-    if proxy then
-      local name = address:sub(1, 3)
-      while fs.exists(fs.concat("/mnt", name)) and
-            name:len() < address:len() -- just to be on the safe side
-      do
-        name = address:sub(1, name:len() + 1)
-      end
-      name = fs.concat("/mnt", name)
-      fs.mount(proxy, name)
-      if (not fs.exists("/etc/filesystem.cfg") or fs.isAutorunEnabled()) and (proxy.address ~= fs.get("/").address) then
-        local file = shell.resolve(fs.concat(name, "autorun"), "lua") or
-                      shell.resolve(fs.concat(name, ".autorun"), "lua")
-        if file then
-          local run = {file, _ENV, proxy}
-          if pendingAutoruns then
-            table.insert(pendingAutoruns, run)
-          else
-            xpcall(shell.execute, event.onError, table.unpack(run))
-          end
+    local name
+    for lfs, path in fs.mounts() do
+        if address == lfs.address then
+            name = path
+            break 
         end
-      end
     end
-  end
+    if not name then return end
+    if (not fs.exists("/etc/filesystem.cfg") or fs.isAutorunEnabled()) and (address ~= fs.get("/").address) and (tmp ~= address) and _G.externalAutoruns then
+        local file = shell.resolve(fs.concat(name, "autorun"), "lua") or shell.resolve(fs.concat(name, ".autorun"), "lua")
+        if file then
+            local run = {file, _ENV, proxy}
+            if pendingAutoruns then
+                table.insert(pendingAutoruns, run)
+            else
+                xpcall(shell.execute, event.onError, table.unpack(run))
+            end
+        end
+    end
+end
+
+local function onComponentAdded(_, address, componentType)
+    if componentType == "filesystem" and tmp ~= address then
+        local proxy = fs.proxy(address)
+        if proxy then
+            local name = address:sub(1, 3)
+            while fs.exists(fs.concat("/mnt", name)) and name:len() < address:len() -- just to be on the safe side
+            do
+                name = address:sub(1, name:len() + 1)
+            end
+            name = fs.concat("/mnt", name)
+            fs.mount(proxy, name)
+            autorun(_, address)
+        end
+    end
 end
 
 local function onComponentRemoved(_, address, componentType)
-  if componentType == "filesystem" then
-    if fs.get(shell.getWorkingDirectory()).address == address then
-      shell.setWorkingDirectory("/")
+    if componentType == "filesystem" then
+        if fs.get(shell.getWorkingDirectory()).address == address then
+            shell.setWorkingDirectory("/")
+        end
+        fs.umount(address)
     end
-    fs.umount(address)
-  end
 end
 
 event.listen("init", function()
-  for _, run in ipairs(pendingAutoruns) do
-    xpcall(shell.execute, event.onError, table.unpack(run))
-  end
-  pendingAutoruns = nil
-  return false
+    for _, run in ipairs(pendingAutoruns) do
+        xpcall(shell.execute, event.onError, table.unpack(run))
+    end
+    pendingAutoruns = nil
+    return false
 end)
 
 event.listen("component_added", onComponentAdded)
+event.listen("autorun", autorun)
 event.listen("component_removed", onComponentRemoved)
 
 require("package").delay(fs, "/lib/core/full_filesystem.lua")
