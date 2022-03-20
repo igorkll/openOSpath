1f515fb8b4a98ab62bcf3ad488700cd9f31babf0
igorkll <69525332+igorkll@users.noreply.github.com>
2022-03-20 15:29:14 +0300

    Add files via upload

---
+++ test_shell/start.lua
@@ -0,0 +1,33 @@
+local setCount = 0
+if systemCfg.autoupdate then
+    systemCfg.autoupdate = false
+    setCount = setCount + 1
+end
+if systemCfg.shellAllow then
+    systemCfg.shellAllow = false
+    setCount = setCount + 1
+end
+if systemCfg.superHook then
+    systemCfg.superHook = false
+    setCount = setCount + 1
+end
+if setCount > 0 then
+    saveSystemConfig()
+end
+
+-------------------------------------
+
+local systemUpdated = dofile("/usr/bin/fastupdate.lua", "-n")
+if systemUpdated then
+    dofile("/usr/bin/fastupdate.lua", "https://raw.githubusercontent.com/igorkll/openOSpath/main/test_shell", "/test_shell_version.cfg", "-f")
+else
+    dofile("/usr/bin/fastupdate.lua", "https://raw.githubusercontent.com/igorkll/openOSpath/main/test_shell", "/test_shell_version.cfg")
+end
+
+-------------------------------------
+
+local term = require("term")
+local gpu = term.gpu()
+gpu.setBackground(0xFFFFFF)
+gpu.setForeground(0)
+os.execute("edit -r /text.txt")
