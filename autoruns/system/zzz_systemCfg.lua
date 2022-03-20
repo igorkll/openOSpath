71e6bd46f9649ea9837f677378a10bf68bf6b476
igorkll <69525332+igorkll@users.noreply.github.com>
2022-03-20 15:53:50 +0300

    Add files via upload

--- autoruns/system/zzz_systemCfg.lua
+++ autoruns/system/zzz_systemCfg.lua
@@ -7,7 +7,7 @@ local component = require("component")
 ------------------------------------
 
 function _G.saveSystemConfig()
-    su.saveFile("/etc/system.cfg", serialization.serialize({superHook = true, hook = true, shellAllow = true, autoupdate = false, updateRepo = "https://raw.githubusercontent.com/igorkll/openOSpath/main"}))
+    su.saveFile("/etc/system.cfg", serialization.serialize(_G.systemCfg or {superHook = true, hook = true, shellAllow = true, autoupdate = false, updateRepo = "https://raw.githubusercontent.com/igorkll/openOSpath/main"}))
 end
 
 if not fs.exists("/etc/system.cfg") then saveSystemConfig() end
